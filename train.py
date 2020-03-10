6ff9dfc9984b9ab609b0ce5bb67de258db3ff2e1
Felipe Sassi Del Sant <felipesassi@outlook.com>
2020-03-10 20:42:45 -0300

    Second commit, insertion of train code

---
+++ train.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from models.model import TL_ResNet50
+from datasets.datasets import generate_train_validation_dataloader
+from models.trainer import Trainer
+from utils.utils import get_device, read_parameters, separate_train_val
+from metrics.metrics import Accuracy_Metric
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/data/train/train_labels.csv")
+    df_train, df_val = separate_train_val(df)
+    configs = read_parameters()
+    device = get_device()
+    ResNet50 = TL_ResNet50(params_dict = configs["network_parameters"], pretrained = True).to(device)
+    train_loader, val_loader = generate_train_validation_dataloader(df_train, 
+                                                                    df_val, 
+                                                                    configs["train_parameters"]["batch_size"], 
+                                                                    "data/data/train/")
+    Loss = nn.BCEWithLogitsLoss()
+    Optimizer = optim.Adam(ResNet50.parameters(),
+                        lr = configs["train_parameters"]["learning_rate"],
+                        weight_decay = configs["train_parameters"]["weight_decay"])
+    Metrics = Accuracy_Metric()
+    Train = Trainer(model = ResNet50,
+                optimizer = Optimizer,
+                loss = Loss,
+                metric = Metrics,
+                train_data = train_gen,
+                validation_data = val_gen,
+                epochs = configs["train_parameters"]["epochs"],
+                device = device,
+                lr_scheduler = None)
+    Train.train()
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn
 import torch.nn.functional as F
 import sys
+import json
+from sklearn.utils import shuffle
 
 def get_device():
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
@@ -16,6 +18,18 @@ def clear_gpu_memory(device="cuda:0"):
     torch.cuda.empty_cache()
     memory_available = torch.cuda.memory_reserved(device)/1024**2
     print("Memory after cleaning: {} MB." .format(memory_available))
+
+def read_parameters():
+  with open("configs/config.json", "r") as file:
+    data = file.read()
+  return json.loads(data)
+
+def separate_train_val(data):
+  data = shuffle(data, random_state = 0).reset_index(drop = True)
+  n = data.shape[0]
+  n1 = int(0.75*n)
+  n2 = n - n1
+  return df[:n1].reset_index(drop = True), df[n1:].reset_index(drop = True)
 
 def show_training_progress(metric, index, batch_size, train):
     if train == False:
